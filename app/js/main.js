3c35ccb248bc6cc8c29d11ae0db3fde12c27ab4a
_vasilie <vasilije.milenkovic@diwanee.com>
2017-03-06 16:38:45 +0100

    new design

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,12 @@
 $(function(){
   $(".b-header-slider").slick({
-
+    prevArrow: '<img src="assets/img/slide-arrow-left.svg" class="arrow-right">',
+    nextArrow: '<img src="assets/img/slide-arrow-right.svg" class="arrow-left">',
+    dots:true
   });
   $(".b-detail-brands-slider").slick({
+    prevArrow: '<img src="assets/img/left-small.svg" class="arrow-right">',
+    nextArrow: '<img src="assets/img/right-small.svg" class="arrow-left">',
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1
