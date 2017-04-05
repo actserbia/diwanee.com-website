f914d552d824747878e8768dc227b4e591ea4750
_vasilie <vasilije.milenkovic@diwanee.com>
2017-04-05 16:29:06 +0200

    detail pages created

--- app/js/main.js
+++ app/js/main.js
@@ -22,7 +22,16 @@ $(function(){
     nextArrow: '<img src="assets/img/right-small.svg" class="arrow-left">',
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    responsive: [{
+      breakpoint:1024,
+      settings: {
+        slidesToShow: 1,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed:3000
+      }
+    }]
 
   });
   $(".menu-ico").click(function(){
